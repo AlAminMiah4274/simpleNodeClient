f709fdc3e8b3624dc31660420f86dcfdbfe7f2f2
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-04 10:51:57 +0600

    tring to learn simple node to make a simple site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  const handleAddUser = e => {
+
+    // to prevent reloading of the form 
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+
+    // to send data to the database
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+
+    console.log(user);
+
+    // to reset the form 
+    e.target.reset();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Name: {users.length}</h1>
+      </div>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='name' required />
+        <br />
+        <input type="email" name="email" id="" placeholder='email' required />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
+
+      <div>
+        {
+          users.map(user => <p key={user.id}> {user.name} Email: {user.email}</p>)
+        }
+      </div>
     </div>
   );
 }
